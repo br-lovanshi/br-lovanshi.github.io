a756c94cd55f3ed27a669a6247b1efdd38bb9c9b
br-lovanshi <brajeshlovanshi2001@gmail.com>
2026-04-19 13:36:50 +0530

    fix: remove conflicting nextjs.yml workflow, drop revalidate from sitemap (incompatible with static build)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,8 +2,6 @@ import { MetadataRoute } from 'next'
 import { client } from '@/lib/sanity.client'
 import { projectsQuery, postsQuery } from '@/lib/queries'
 
-export const revalidate = 60;
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = 'https://br-lovanshi.github.io'
 
